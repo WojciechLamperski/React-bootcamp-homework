ebf469d76e388ab4ac927d85880bb1c841ff0868
Wojciech Lamperski <wolamp20@gmail.com>
2022-10-11 01:18:00 +0200

    adding some test content :)

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import reactLogo from '../src/logo.svg';
 function App() {
   return (
     <div className="app">
-      < Header text="Header" logosrc={reactLogo} />
+      < Header text="Deleting users from list - The Website" logosrc={reactLogo} />
       <div className='wrapper'>
         < Content >          
         </Content >
-        < Sidebar text="Sidebar" />
+        < Sidebar text="This website allows you to delete users from list, but you shouldn't do it someone put time and effort into placing them there, please show respect and don't input users name into that big input field below websites name BUT if you by any chance happen to do that calm down because its still not the end of the world ;) to delete the user you also have to click the DELETE button, so you can relax if you selected a user, as long as you do not click that big button that says DELETE we should all be able to be fine :) Ahh just grasp the beauty and harmony of this website with all the users intact :D" />
       </div>
       < Footer text="Footer" />
     </div>
