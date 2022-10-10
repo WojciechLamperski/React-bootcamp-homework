a9d010cb5777c3d92fa3c630238e926d74809ca8
Wojciech Lamperski <wolamp20@gmail.com>
2022-10-10 21:36:11 +0200

    added header, sidebar, content, footer to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { Header } from './components/Header';
+import { Content } from './components/Content';
+import { Sidebar } from './components/Sidebar';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="app">
-      Put solution here
+      < Header />
+      < Content />
+      < Sidebar />
+      < Footer />
     </div>
   );
 }
 
 export default App;
+
+    
+
---
+++ src/components/Content.js
@@ -0,0 +1,7 @@
+export const Content = () =>{
+    return(
+        <main>
+            Content
+        </main>
+    );
+}
