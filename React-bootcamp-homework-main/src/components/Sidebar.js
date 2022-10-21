bb8add7bb14832604dc409d1875def9df097b562
Wojciech Lamperski <wolamp20@gmail.com>
2022-10-21 22:33:00 +0200

    changed the description

--- React-bootcamp-homework-main/src/components/Sidebar.js
+++ React-bootcamp-homework-main/src/components/Sidebar.js
@@ -1,11 +1,17 @@
 import { StyledSidebar } from "./styles/Sidebar.styled";
 
 export const Sidebar = ({ text, buttonOnClick, search }) =>{
+    
+    const searchName = document.querySelector('input');
+    function search() {
+        console.log(searchName)
+    } 
+
     return(
         <StyledSidebar buttonOnClick ={ buttonOnClick } inputOnChange = { search }>
             { text }
             <button onClick = { buttonOnClick } >Change dark/light mode</button>
-            <input type="text" placeholder='search' onClick={ search }></input>
+            <input type="text" placeholder='search' onChange = { search }></input>
         </StyledSidebar>
     );
 }
